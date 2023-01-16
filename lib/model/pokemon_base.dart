39a56b06f59a30d1dc8b9a93dc3f55238b72a9b5
maxvyr <maxvid30@gmail.com>
2023-01-16 21:22:08 +0100

    ✨ update model with json annotation

--- lib/model/pokemon_base.dart
+++ lib/model/pokemon_base.dart
@@ -1,19 +1,20 @@
 import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+part 'pokemon_base.g.dart';
 
+@JsonSerializable()
 class Pokemon extends Equatable {
   const Pokemon({
     required this.name,
     required this.url,
   });
 
-  factory Pokemon.fromJson(Map<String, dynamic> json) {
-    return Pokemon(
-      name: json["name"],
-      url: json["url"],
-    );
-  }
+  factory Pokemon.fromJson(Map<String, dynamic> json) => _$PokemonFromJson(json);
+  Map<String, dynamic> toJson() => _$PokemonToJson(this);
 
+  @JsonKey(defaultValue: "")
   final String name;
+  @JsonKey(defaultValue: "")
   final String url;
 
   @override
